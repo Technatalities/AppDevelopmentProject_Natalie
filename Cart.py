da31cc258406141ad3a3784782e10085a91b9028
Technatalities <natalie.sophie.chua@gmail.com>
2023-02-08 13:03:21 +0800

    Adding cart forms for quantity

--- Cart.py
+++ Cart.py
@@ -4,7 +4,7 @@ import shelve
 class Cart:
     def __init__(self, id):
         self.__id = id
-        self.__items = [] # Contains product IDs and quanitty
+        self.__items = [] # Contains product and quanitty
 
     def set_id(self, id):
         self.__id = id
@@ -19,18 +19,19 @@ class Cart:
         return self.__items
 
     def set_item(self, cartItem):
-        self.__items.add(cartItem)
+        self.__items.append(cartItem)
 
 class CartItem:
-    def __init__(self, product_id, quantity):
-        self.__product_id = product_id
+    def __init__(self, product, quantity):
+        self.__product = product # Stores all product information here
         self.__quantity = quantity
 
-    def set_product_id(self, product_id):
-        self.__product_id = product_id
+# get.name from product from Khansa's class.
+    def set_product(self, product):
+        self.__product = product
 
-    def get_product_id(self):
-        return self.__product_id
+    def get_product(self):
+        return self.__product
 
     def set_quantity(self, quantity):
         self.__quantity = quantity
@@ -38,7 +39,7 @@ class CartItem:
     def get_quantity(self):
         return self.__quantity
 
-def get_product(prodcut_key):
+def get_product(product_key):
     product = Product("Milk", 200, "Food", 3.99, 4.3, "")
     return product
 
