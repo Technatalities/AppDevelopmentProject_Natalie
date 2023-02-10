88b8dfb7d92fe447d76d036cb03a81c703cb07bf
Technatalities <natalie.sophie.chua@gmail.com>
2023-02-11 00:02:47 +0800

    Updating stuff ;-; Removing old redundant code

--- __init__.py
+++ __init__.py
@@ -72,60 +72,6 @@ def remove_from_wishlist(id):
     wishlist.remove_wl_item(product)
     save_wishlist(wishlist)
     return render_template('displayWishlist.html', count=wishlist.get_count(), wishlist=wishlist)
-
-# @app.route('/displayCart', methods=['GET', 'POST'])
-# def display_cart():
-#     cart_db = shelve.open('shopping_cart.db', 'c')
-#     print(cart_db)
-#     cart_list = []
-#     for key in cart_db:
-#         item = cart_db.get(key)
-#         cart_list.append(item)
-#     cart_db.close()
-#     return render_template('displayCart.html', count=len(cart_list), cart_list=cart_list)
-#
-#
-# @app.route('/addQuantity/<int:id>', methods=['POST', 'GET'])
-# def add_quantity(id):
-#     product_key = str(id)
-#     cart_db = shelve.open("shopping_cart.db", flag='c')
-#     cart = cart_db['Cart'] if 'Cart' in cart_db else {}
-#     if product_key in cart:
-#         cart[product_key][int('quantity')] += 1
-#         cart_db['Cart'] = cart
-#         cart_db.close()
-#         return "Cart updated successfully"
-#     else:
-#         cart_db.close()
-#         return "Error: Invalid product key"
-#
-# @app.route('/subtractQuantity/<int:id>', methods=['POST', 'GET'])
-# def subtract_quantity(id):
-#     product_key = str(id)
-#     cart_db = shelve.open("shopping_cart.db", flag='c')
-#     cart = cart_db['Cart'] if 'Cart' in cart_db else {}
-#     if product_key in cart:
-#         cart[product_key][int('quantity')] -= 1
-#         cart_db['Cart'] = cart
-#         cart_db.close()
-#         return "Cart updated successfully"
-#     else:
-#         cart_db.close()
-#         return "Error: Invalid product key"
-#
-# @app.route('/delete_cart/<int:id>', methods = ["POST", "GET"])
-# def remove_from_cart(id):
-#     product_key = str(id)
-#     print(product_key)
-#     cart_db = shelve.open("shopping_cart.db", flag='c')
-#     if product_key in cart_db.keys():
-#         print(cart_db.keys())
-#         del cart_db[product_key]
-#         cart_db.close()
-#         return redirect(url_for("display_cart"))
-#     else:
-#         cart_db.close()
-#         return "Error: Invalid product key"
 #
 # @app.route('/invoice', methods=['GET', 'POST'])
 # def invoice():
